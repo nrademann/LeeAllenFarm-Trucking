bef43aa08cb76100e9ad79e1166631e04a1f4b3e
nick rademann <nrademann@hotmail.com>
2022-07-11 22:59:33 -0500

    ViewModels

---
+++ ViewModels/ClientLoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LeeAllenFarm_Trucking.ViewModels
+{
+    public class ClientLoginViewModel
+    {
+        [Required, MaxLength(256), EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+        [Required, MinLength(6), MaxLength(20)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Required, MinLength(6), MaxLength(20)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
