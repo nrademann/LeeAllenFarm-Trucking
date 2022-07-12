ba4f3279bbd697b5662b26dd858742c24a2e7474
nick rademann <nrademann@hotmail.com>
2022-07-11 21:48:06 -0500

    ClientLogin

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using LeeAllenFarmAndTrucking.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LeeAllenFarmAndTrucking.Controllers
+{
+    public class AccountController : Controller
+    {
+        private ClientDbContext db;
+        private UserManager<ClientInfo> userManager;
+        private SignInManager<ClientInfo> signInManager;
+        private RoleManager<ClientInfo> roleManager;
+        public AccountController(UserManager<ClientInfo> userManager,
+            SignInManager<ClientInfo> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            ClientDbContext db)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+ //           this.roleManager = roleManager;
+            this.db = db;
+        }
+        [Authorize]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+
+}
